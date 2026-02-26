047c4300a51229d085c9b97cfb6a5e8811d0f929
DuongNT <tungduong7324@gmail.com>
2026-02-26 18:03:28 +0700

    Update: Fix EstatusType Enum

--- Assets/SEP490/Gameplay/Shared/Status/Scripts/EStatusType.cs
+++ Assets/SEP490/Gameplay/Shared/Status/Scripts/EStatusType.cs
@@ -6,7 +6,7 @@ namespace SEP490G69
         Power = 1,
         Intelligence = 2,
         Defense = 3,
-        Speed = 4,
+        Agi = 4,
         Vitality = 5,
         Energy = 6,
         Mood = 7,
